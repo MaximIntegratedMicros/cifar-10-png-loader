397fa720dc4a247a9bbc0f97aaaf185d87a7ae19
Keyur.Hariya <keyur.hariya@maximintegrated.com>
2021-01-12 18:45:15 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# Draft Instructions
+
+## Preparing the MAX78000EVKIT board
+* Build and load the program 'cifar-10-auto-test' on the MAX78000 EVKIT/Feather board: https://github.com/MaximIntegratedAI/MaximAI_Documentation/blob/master/MAX78000_Evaluation_Kit/README.md#building-the-sdk-examples
+
+
+## Preparing the host machine
+(Skip any step where requirement is already met)
+1. Install Python 3.x. Python 2.x is incompatible.
+2. Install pip: https://pip.pypa.io/en/stable/installing/
+3. Install and create the virutal environment: https://packaging.python.org/guides/installing-using-pip-and-virtual-environments/#installing-virtualenv
+4. Clone the github repository: `$ git clone <link_to_repo>`
+5. Navigate to the repository: `$ cd <repo_name>`
+6. Activate the virtual environment: https://packaging.python.org/guides/installing-using-pip-and-virtual-environments/#activating-a-virtual-environment
+7. Install the requirements for this project: `$ pip install -r requirements.txt`
+8. Open the file main.py in a code editor and change the 'serial_dev' variable if required.
+8. Store the test images in 32x32 RGB .png format in a directory.
+9. Make sure the MAX78000EVKIT is powered on and running the 'cifar-10-auto-test' example, then run the python script: `$ python main.py <path_to_test_images>`
+
+### Expected Output
+file1.png - Class:0
+
+file2.png - Class:9
---
+++ main.py
@@ -0,0 +1,84 @@
+import os
+import sys
+import serial
+import ft4222
+import ft4222.SPI as SPI
+import ft4222.SPIMaster as SPIM
+import PIL.Image as Image
+
+# Serial device and baud rate for MAX78000
+serial_dev = r'/dev/ttyUSB0'
+serial_baud = 115200
+
+def print_result(filename, result):
+    print("{0} - {1}".format(filename, result))
+
+
+def main():
+
+    if len(sys.argv) < 2:
+        print("ERROR Missing argument: path to test images")
+        sys.exit()
+
+    abs_path = os.path.realpath(sys.argv[1])
+    if not os.path.exists(abs_path):
+        print("ERROR Diretory does not exists: {0}".format(abs_path))
+        sys.exit()
+
+    print("\nTesting images from: {0}\n".format(abs_path))
+
+    # Open device with default description
+    MAX78000FTDI = ft4222.openByDescription('FT4222 A')
+
+    MAX78000FTDI.spiMaster_Init(SPIM.Mode.SINGLE,
+                                SPIM.Clock.DIV_8, # Must match the SPI Clock on MAX78000
+                                SPI.Cpol.IDLE_LOW,
+                                SPI.Cpha.CLK_LEADING,
+                                SPIM.SlaveSelect.SS0)
+
+    # Serial port must be open before end of SPI transaction to avoid missing characters
+    MAX78000Serial = serial.Serial(serial_dev, serial_baud, timeout=5)
+
+    for filename in os.listdir(abs_path):
+        # Ignore all other files
+        if not filename.endswith('.png'):
+            continue
+
+        with Image.open(os.path.join(abs_path, filename)) as img:
+            if not img.mode == 'RGB':
+                print_result(filename, "Skipped, image type must be RGB\n")
+                continue
+
+            if not img.size == (32,32):
+                print_result(filename, "Skipped, image size must be 32x32\n")
+                continue
+
+            image_data = img.getdata()
+
+        # Convert list of tuples to flat list
+        image_data = [color_value for pixel in image_data for color_value in pixel]
+
+        # Send image to MAX78000 SPI slave
+        sent_bytes = MAX78000FTDI.spiMaster_SingleWrite(bytes(image_data), False)
+
+        # Wait for test result of image
+        # read() blocks until timeout
+        result = []
+        while 1:
+            char = MAX78000Serial.read(1)
+            if char == b'':
+                result = "Timeout"
+                break
+            result.append(char.decode('utf-8'))
+            if char == b'\n':
+                result = "".join(result)
+                break
+
+        print_result(filename, result)
+
+    MAX78000Serial.close()
+    MAX78000FTDI.close()
+
+
+if __name__ == "__main__":
+    main()
