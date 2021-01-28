a9067767d7026a6a5697ccddbf0467afce7b4065
Keyur.Hariya <keyur.hariya@maximintegrated.com>
2021-01-28 15:58:35 -0600

    Cleanup
    Better print format
    Avoid magic numbers
    Sorted images
    Remove unwanted comment

--- main.py
+++ main.py
@@ -6,12 +6,19 @@ import ft4222.SPI as SPI
 import ft4222.SPIMaster as SPIM
 import PIL.Image as Image
 
-# Serial device and baud rate for MAX78000
+# Start - Match these with MAX78000 application
+SERIAL_BAUD = 115200
+IMG_WIDTH   = 32
+IMG_HEIGHT  = 32
+# End
+
+SERIAL_TOUT = 5     # Seconds
+IMG_MODE    = "RGB"
+
 serial_dev = r'/dev/ttyUSB0'
-serial_baud = 115200
 
 def print_result(filename, result):
-    print("{0} - {1}".format(filename, result))
+    print("{0}\t{1}".format(filename, result), end='')
 
 
 def main():
@@ -31,26 +38,26 @@ def main():
     MAX78000FTDI = ft4222.openByDescription('FT4222 A')
 
     MAX78000FTDI.spiMaster_Init(SPIM.Mode.SINGLE,
-                                SPIM.Clock.DIV_8, # Must match the SPI Clock on MAX78000
+                                SPIM.Clock.DIV_8,
                                 SPI.Cpol.IDLE_LOW,
                                 SPI.Cpha.CLK_LEADING,
                                 SPIM.SlaveSelect.SS0)
 
     # Serial port must be open before end of SPI transaction to avoid missing characters
-    MAX78000Serial = serial.Serial(serial_dev, serial_baud, timeout=5)
+    MAX78000Serial = serial.Serial(serial_dev, SERIAL_BAUD, timeout=SERIAL_TOUT)
 
-    for filename in os.listdir(abs_path):
+    for filename in sorted(os.listdir(abs_path)):
         # Ignore all other files
         if not filename.endswith('.png'):
             continue
 
         with Image.open(os.path.join(abs_path, filename)) as img:
-            if not img.mode == 'RGB':
-                print_result(filename, "Skipped, image type must be RGB\n")
+            if not img.mode == IMG_MODE:
+                print_result(filename, "Skipped, image mode must be {0}\n".format(IMG_MODE))
                 continue
 
-            if not img.size == (32,32):
-                print_result(filename, "Skipped, image size must be 32x32\n")
+            if not img.size == (IMG_WIDTH, IMG_HEIGHT):
+                print_result(filename, "Skipped, image size must be {0}x{1}\n".format(IMG_WIDTH, IMG_HEIGHT))
                 continue
 
             image_data = img.getdata()
@@ -61,8 +68,8 @@ def main():
         # Send image to MAX78000 SPI slave
         sent_bytes = MAX78000FTDI.spiMaster_SingleWrite(bytes(image_data), True)
 
-        # Wait for test result of image
-        # read() blocks until timeout
+        # Wait for the test result
+        # read() blocks until SERIAL_TOUT
         result = []
         while 1:
             char = MAX78000Serial.read(1)
