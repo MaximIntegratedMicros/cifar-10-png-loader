d9627dc783bc28ba18857c6115a3e569b4e59132
Jesse Marroquin <jesse.marroquin@maximintegrated.com>
2021-04-21 14:01:00 -0500

    Update for release

--- README.md
+++ README.md
@@ -1,23 +1,90 @@
-# Draft Instructions
+# MAX78000 CIFAR-10 Auto Test Example
 
 ## Preparing the MAX78000EVKIT board
-* Build and load the program 'cifar-10-auto-test' on the MAX78000 EVKIT/Feather board: https://github.com/MaximIntegratedAI/MaximAI_Documentation/blob/master/MAX78000_Evaluation_Kit/README.md#building-the-sdk-examples
+#### Clone the upstream firmware project
+
+```
+$ cd <maxim-sdk-directory>/Examples/MAX78000/CNN
+$ git clone https://github.com/MaximIntegratedMicros/cifar-10-auto-test.git
+```
+
+#### Build and load the `cifar-10-auto-test` firmware following the 'Building the SDK Examples' document
+
+ https://github.com/MaximIntegratedAI/MaximAI_Documentation/blob/master/MAX78000_Evaluation_Kit/README.md#building-the-sdk-examples
+
+#### Configure MAX78000EVKIT for running cifar-10-auto-test:
+
+* Turn MAX78000EVKIT off by placing SW1 in the OFF position
+* Remove camera module connected to J4 if installed
+* De-select camera interface and select USB-SPI interface by installing jumper on JP22 pins 1 and 2
+* Connect host to MAX78000EVKIT CN1 using USB Micro-B cable
+* Connect host to MAX78000EVKIT CN2 using USB Micro-B cable
+* Turn MAX78000EVKIT on by placing SW1 in the ON position
 
 
 ## Preparing the host machine
-(Skip any step where requirement is already met)
-1. Install Python 3.7 or 3.8. Python 2.x and 3.9 are incompatible.
-2. Install pip: https://pip.pypa.io/en/stable/installing/
-3. Install the virutal environment: https://packaging.python.org/guides/installing-using-pip-and-virtual-environments/#installing-virtualenv
-4. Clone the github repository: `$ git clone <link_to_repo>`
-5. Navigate to the repository: `$ cd <repo_name>`
-6. Create and activate the virtual environment: https://packaging.python.org/guides/installing-using-pip-and-virtual-environments/#creating-a-virtual-environment
-7. Install the requirements for this project: `$ pip install -r requirements.txt`
-8. Open the file main.py in a code editor and change the 'serial_dev' variable if required.
-8. Store the test images in 32x32 RGB .png format in a directory.
-9. Make sure the MAX78000EVKIT is powered on and running the 'cifar-10-auto-test' example, then run the python script: `$ python main.py <path_to_test_images>`
+The host application communicates with the MAX78000EVKIT using two USB interfaces, a USB-to-UART bridge and a USB-to-QSPI bridge.
 
-### Expected Output
-file1.png - Class:0
+The host application transfers image data to the MAX78000 using the QSPI bridge (Quad Serial Peripheral Interface). Connector CN2 is used to connect to the MAX78000EVKIT USB-to-QSPI bridge.
 
-file2.png - Class:9
+The host application receives MAX78000 inference results using the UART bridge (Universal Asynchronous Receiver/Transmitter). Connector CN1 is used to connect to the MAX78000EVKIT USB-to-UART bridge.
+
+The host application is written in Python and uses the following packages:
+
+- pyserial -- USB-to-UART communications
+- ft4222 -- USB-to-QSPI communications
+- Pillow --  image data access
+
+The the host application requires Python 3.7.x or 3.8.x.  Python 2.x and 3.9 are not supported.
+
+###### It is not necessary to install a compatible Python system-wide, or to rely on the system-provided Python. To manage Python versions, use `pyenv` (https://github.com/pyenv/pyenv).
+
+###### Like the Python interpreter, Python packages do not need to be installed system-wide, or to rely on the system-provided Python packages. To manage external Python packages, use `pip` (https://pip.pypa.io/en/stable/installing/). Note that pip is installed with Python >= 3.4 downloaded from [python.org]()  or when working in a Virtual Environment created by `venv`.
+
+#### Creating project directory and cloning upstream project
+
+```$ mkdir ai85project
+$ mkdir ai85proj
+$ cd ai85proj
+$ git clone https://github.com/MaximIntegratedMicros/cifar-10-png-loader
+```
+
+#### Creating the Virtual Environment
+
+```
+$ cd cifar-10-png-loader
+$ pyenv local 3.8.6
+$ python3 -m venv .
+$ source bin/activate
+```
+
+#### Installing Python modules
+
+```
+$ pip3 install -r requirements.txt
+```
+
+## Running the example
+
+With the MAX78000EVKIT connected, powered on and running the 'cifar-10-auto-test' example, run the host application.
+
+```
+$ python main.py --device /dev/ttyUSB0 --path test-images
+```
+
+Where `--device` specifies the serial port associated with the MAX78000EVKIT USB-to-UART bridge and `--path` specifies the directory containing the test images.
+
+The expected output is shown here:
+
+```
+
+Testing images from: /home/fwlogin/develop/ai85proj/cifar-10-png-loader/test-images
+
+airplane1.png	Class:0
+airplane10.png	Class:0
+airplane2.png	Class:0
+airplane3.png	Class:0
+airplane4.png	Class:0
+...
+```
+
--- main.py
+++ main.py
@@ -1,11 +1,38 @@
+###################################################################################################
+#
+# Copyright (C) Maxim Integrated Products, Inc. All Rights Reserved.
+#
+# Maxim Integrated Products, Inc. Default Copyright Notice:
+# https://www.maximintegrated.com/en/aboutus/legal/copyrights.html
+#
+###################################################################################################
+#
+
+'''
+    This is an example application executing remote
+    cifar-10 inferences on a MAX78000 evaluation kit.
+
+    CIFAR-10 classifications by index:
+    0 airplane
+    1 automobile
+    2 bird
+    3 cat
+    4 deer
+    5 dog
+    6 frog
+    7 horse
+    8 ship
+    9 truck
+'''
+
 import os
 import sys
+import argparse
 import serial
 import ft4222
 import ft4222.SPI as SPI
 import ft4222.SPIMaster as SPIM
 import PIL.Image as Image
-import argparse
 
 # Start - Match these with MAX78000 application
 SERIAL_BAUD = 115200
@@ -18,39 +45,43 @@ IMG_MODE    = "RGB"
 
 
 def print_result(filename, result):
-    print("{0}\t{1}".format(filename, result), end='')
+    """ Print formatted result """
+    print("{0}\t{1}".format(filename, result), end = '')
 
 
 def main():
-    parser = argparse.ArgumentParser(description = "Recursively test PNG images with cifar-10 based example on MAX78000.",
-                                     formatter_class= argparse.RawTextHelpFormatter)
+    """ main function """
+    parser = argparse.ArgumentParser(description = "Test PNG images with cifar-10 based example.",
+                                     formatter_class = argparse.RawTextHelpFormatter)
     parser.add_argument("-p", "--path",
-                        required=True,
-                        help="Relative or absolute path to the directory of test images.")
+                        required = True,
+                        help = "Relative or absolute path to the directory of test images.")
     parser.add_argument("-d", "--device",
-                        required=True,
-                        help="Serial device connected to MAX78000.\nUnix: /dev/ttyUSB0\nWindows: COM1")
+                        required = True,
+                        help = "Serial device connected to MAX78000.\n"
+                               "Linux: /dev/ttyUSB0\n"
+                               "Windows: COM1")
 
     args = parser.parse_args()
 
     abs_path = os.path.realpath(args.path)
     if not os.path.exists(abs_path):
-        print("ERROR Diretory does not exists: {0}".format(abs_path))
+        print("ERROR Directory does not exists: {0}".format(abs_path))
         sys.exit()
 
     print("\nTesting images from: {0}\n".format(abs_path))
 
     # Open device with default description
-    MAX78000FTDI = ft4222.openByDescription('FT4222 A')
+    spim = ft4222.openByDescription('FT4222 A')
 
-    MAX78000FTDI.spiMaster_Init(SPIM.Mode.SINGLE,
-                                SPIM.Clock.DIV_8,
-                                SPI.Cpol.IDLE_LOW,
-                                SPI.Cpha.CLK_LEADING,
-                                SPIM.SlaveSelect.SS0)
+    spim.spiMaster_Init(SPIM.Mode.SINGLE,
+                        SPIM.Clock.DIV_8,
+                        SPI.Cpol.IDLE_LOW,
+                        SPI.Cpha.CLK_LEADING,
+                        SPIM.SlaveSelect.SS0)
 
     # Serial port must be open before end of SPI transaction to avoid missing characters
-    MAX78000Serial = serial.Serial(args.device, SERIAL_BAUD, timeout=SERIAL_TOUT)
+    sport = serial.Serial(args.device, SERIAL_BAUD, timeout = SERIAL_TOUT)
 
     for filename in sorted(os.listdir(abs_path)):
         # Ignore all other files
@@ -59,11 +90,13 @@ def main():
 
         with Image.open(os.path.join(abs_path, filename)) as img:
             if not img.mode == IMG_MODE:
-                print_result(filename, "Skipped, image mode must be {0}\n".format(IMG_MODE))
+                print_result(filename,
+                             "Skipped, image mode must be {0}\n".format(IMG_MODE))
                 continue
 
             if not img.size == (IMG_WIDTH, IMG_HEIGHT):
-                print_result(filename, "Skipped, image size must be {0}x{1}\n".format(IMG_WIDTH, IMG_HEIGHT))
+                print_result(filename,
+                             "Skipped, image size must be {0}x{1}\n".format(IMG_WIDTH, IMG_HEIGHT))
                 continue
 
             image_data = img.getdata()
@@ -72,13 +105,13 @@ def main():
         image_data = [color_value for pixel in image_data for color_value in pixel]
 
         # Send image to MAX78000 SPI slave
-        sent_bytes = MAX78000FTDI.spiMaster_SingleWrite(bytes(image_data), True)
+        _ = spim.spiMaster_SingleWrite(bytes(image_data), True)
 
         # Wait for the test result
         # read() blocks until SERIAL_TOUT
         result = []
         while 1:
-            char = MAX78000Serial.read(1)
+            char = sport.read(1)
             if char == b'':
                 result = "Timeout"
                 break
@@ -89,8 +122,8 @@ def main():
 
         print_result(filename, result)
 
-    MAX78000Serial.close()
-    MAX78000FTDI.close()
+    sport.close()
+    spim.close()
 
 
 if __name__ == "__main__":
